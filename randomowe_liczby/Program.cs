0cbb8769729d943fcfd2ebaa5edf32cbc41b3554
Mari0 <discordtako8@gmail.com>
2024-05-30 00:35:41 +0200

    Dodaj pliki projektów.

---
+++ randomowe_liczby/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace randomNumber
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Console.WriteLine("Zgadnij o jakiej liczbie myślę!!! (1-100)");
+            
+            var random = new Random();
+            var number = random.Next(1, 101);
+            var userInput = 0;
+            
+            while (userInput != number)
+            {
+                userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput > number)
+                {
+                    Console.WriteLine("Moja liczba jest mniejsza :3");
+                }
+                if (userInput < number)
+                {
+                    Console.WriteLine("Moja liczba jest wieksza :3");
+                }
+                if (userInput == number) 
+                {
+                    Console.WriteLine("Udało ci sie odgadnąć :3!!!");
+                }
+            }
+        }
+    }
+}
